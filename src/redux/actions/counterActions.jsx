69fec700830994b412aee32ee19960047c360d07
zeynepyilmaz <ylmzbhr1@gmail.com>
2025-02-04 00:28:23 +0300

    Add initial implementation of Todo  components with Redux integration.

---
+++ src/redux/actions/counterActions.jsx
@@ -0,0 +1,5 @@
+
+export const arttır = () => ({type:"INCREASE", payload1:"ARTTIRILDI"})
+export const sıfırla= () => ({type:"RESET", payload1:"SIIFIRLANDI"})
+export const azalt= () => ({type:"DECREASE", payload1:"AZALTILDI"})
+
