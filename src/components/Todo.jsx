7feff3bc26f51c386eb7a41e8ee9499424110ae6
zeynepyilmaz <ylmzbhr1@gmail.com>
2025-02-05 13:56:52 +0300

    Enhance Todo component styling: add global reset styles, include icon in header, and adjust CSS for better layout

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,13 +1,19 @@
 import TodoInput from "./TodoInput";
 import TodoList from "./TodoList";
 import "./Todo.css";
+import todolisticon from "../assets/todolisticon.png";
 
 const Todo = () => {
   return (
     <div>
-        <h2 className="todo-head">TO DO LIST</h2>
-        <TodoInput />
-        <TodoList />
+      <h2 className="todo-head">
+        TO DO LIST
+        <span>
+          <img className="todo-icon" src={todolisticon} alt="" />
+        </span>
+      </h2>
+      <TodoInput />
+      <TodoList />
     </div>
   );
 };
